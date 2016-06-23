f5138599ccc86f41a47cb629ff71a82375ff4f45
wakawaka54 <soue7@hotmail.com>
2016-06-22 21:42:54 -0500

    Rough implementation of Microsoft Identity - Distracted :sad:

--- LiveMessages/Startup.cs
+++ LiveMessages/Startup.cs
@@ -10,7 +10,12 @@ using System.Web.Http;
 
 using Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
 
+using Microsoft.AspNet.Identity;
+
+using LiveMessages.Infrastructure;
 
 [assembly: OwinStartup(typeof(LiveMessages.Startup))]
 
@@ -21,6 +26,14 @@ namespace LiveMessages
         public void Configuration(IAppBuilder app)
         {
             app.MapSignalR();
+
+            app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/User/Login"),
+            });
 
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
